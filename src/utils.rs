3bb94120a1fff36195e39fae8a978bdfb7dc3b63
Sebastian <Sebastian@Strobl.net>
2020-06-28 04:48:14 +0200

    Add: command to praise ferris

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use serenity::model::channel::Message;
+use serenity::model::channel::{Message, Embed};
 use serenity::prelude::{Context};
 use serenity::framework::standard::{
     CommandResult,
@@ -7,13 +7,27 @@ use serenity::framework::standard::{
         group
     }
 };
+use serenity::utils::MessageBuilder;
 
 #[group]
-#[commands(ping)]
+#[commands(ping, ferris)]
 struct Utils;
 
 #[command]
 fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!")?;
+    Ok(())
+}
+
+#[command]
+fn ferris(ctx: &mut Context, msg: &Message) -> CommandResult {
+    if let Err(why) = msg.channel_id.send_message(&ctx.http, |m| {
+        m.embed(|e| {
+            e.title("Thank you for praising Ferris and being a cool Rustacean, who enjoys writing in the best language on earth!").image("https://rustacean.net/assets/rustacean-flat-happy.png")
+        })
+    }) {
+        println!("Error sending message: {:?}", why);
+    }
+
     Ok(())
 }
